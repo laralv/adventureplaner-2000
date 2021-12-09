89a7302c936e9eaf32e9dc9ce3e582362506a798
Xivind <oivind.langeland@gmail.com>
2021-12-09 07:12:58 +0100

    Added gspread

--- README.md
+++ README.md
@@ -4,5 +4,10 @@ Script for å dra ruteinformasjon ut av Strava og samle den i et google docs.
 
 Google Sheet API, not Google Drive API
 
+Set up service account first
+Create the sheet
+Use gspread
+Setting up oauth
+
 <img width="500" src="https://raw.githubusercontent.com/laralv/adventureplaner-2000/master/URN%20NBN%20no-nb_foto_NF_WL_02381.jpg" />
 Tidlige bikepackere på Voss (foto: Axel Lindahl/Norsk Folkemuseum)
--- test.py
+++ test.py
@@ -2,12 +2,24 @@
 
 import gspread
 
-gc = gspread.service_account()
 
-workbook = gc.open_by_key('1LOXibiJnqvVGRGNz4nnKL9FiWtRmnj3hyjO1dqSlnN0')
-worksheet = workbook.worksheet("Test")
+class GoogleSheets():
+    gc = gspread.service_account()
+    workbook = gc.open_by_key('1LOXibiJnqvVGRGNz4nnKL9FiWtRmnj3hyjO1dqSlnN0') #move to options
+    worksheet = workbook.worksheet("Test")
 
-x = 1
-while x < 20:
-    worksheet.update(f'B{x}', f'test {x}')
-    x = x+1
+    def update_spreadsheet(self):
+
+class Komoot():
+    
+
+class DataProcessor():
+
+
+
+    """
+    x = 1
+    while x < 20:
+        worksheet.update(f'C{x}', f'test {x}')
+        x = x+1
+    """
