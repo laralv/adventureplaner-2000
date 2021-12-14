724380ddbf0e47ce9e05d423a64d1627fb749564
Xivind <oivind.langeland@gmail.com>
2021-12-14 22:51:21 +0100

    Work on google class

--- test.py
+++ test.py
@@ -16,14 +16,22 @@ class GoogleSheets:
         self.workbook = self.service_account.open_by_key('1LOXibiJnqvVGRGNz4nnKL9FiWtRmnj3hyjO1dqSlnN0') #move to options
         self.worksheet = self.workbook.worksheet("Norge på langs") #move to options
         self.route_ids = list()
+        self.column_id = list()
         self.read_route_ids()
+        self.find_column_ids()
     
-    def read_google_config(self):
+    def read_google_config(self): #dont need in first version
         pass
+    
+    def find_column_ids(self):
+        test = self.worksheet.find("Route ID (M)") #Create a list here, or dictionary
+        self.column_id.append(test.row, test.col)??
+        test.
+        pass #store info in a dictionary
     
     def read_route_ids(self):
         print(f'{datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}: Reading route IDs')
-        self.route_ids = self.worksheet.col_values(1)
+        self.route_ids = self.worksheet.col_values(1) #refer instead to method column variables, see above
         for text in range(4): #check if this can be done in a better way, while loop? Maybe also if the id were converted to int, all strings could be deleted?
             self.route_ids.pop(0)
         ic(self.route_ids)
@@ -35,8 +43,26 @@ class GoogleSheets:
             route_data = aggregated_route_data.get(route_id)
             for item in route_data:
                 print(item)
-                self.worksheet.batch_update() # here now, build list of dictionaries , but not loop
-                #here now, use 
+                 
+                row_id = self.worksheet.find(route_id)
+                test = row_id.row
+            
+                self.worksheet.batch_update([{
+                    'range': f'{self.column_id[0]}{row_id.row}:{self.column_id[0]}{row_id}',
+                    'values': [[route_data[1]]],
+                }, {
+                    'range': f'{self.column_id[1]}{row_id}:{self.column_id[1]}{row_id}',
+                    'values': [[route_data[2]]],
+                }])
+
+
+                worksheet.batch_update([{
+                    'range': 'A1:B1',
+                    'values': [['42', '43']],
+                }, {
+                    'range': 'my_range',
+                    'values': [['44', '45']],
+                }])
 
     def test_write(self): #remove this one
         ic()
@@ -67,14 +93,18 @@ class Authenticator:
             
     def get_new_access_token(self):
         print(f'{datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}: Getting new access token')
-        token_url = "https://www.strava.com/oauth/token"
-        response = requests.post(url = token_url,data =\
-            {'client_id': self.client_id,'client_secret': self.client_secret,'grant_type': 'refresh_token','refresh_token': self.refresh_token})
-        response_json = response.json()
-        self.access_token = str(response_json['access_token'])
-        self.refresh_token = str(response_json['refresh_token'])
-        ic(self.access_token, self.refresh_token)
-        self.write_secrets()
+        try:
+            token_url = "https://www.strava.com/oauth/token"
+            response = requests.post(url = token_url,data =\
+                {'client_id': self.client_id,'client_secret': self.client_secret,'grant_type': 'refresh_token','refresh_token': self.refresh_token})
+            response_json = response.json()
+            self.access_token = str(response_json['access_token'])
+            self.refresh_token = str(response_json['refresh_token'])
+            ic(self.access_token, self.refresh_token)
+            self.api_status = response.status_code #check on next run with outdated token if this will work, should return 200
+            self.write_secrets()
+        except:
+            print(f'{datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}: Error getting new access token')
 
     def write_secrets(self):
         print(f'{datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}: Writing new secrets to file')
@@ -116,7 +146,6 @@ class Strava:
         elif self.api_status == 401:
             print(f'{datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}: Access token expired - API Code {self.api_status}')
             self.authenticator.get_new_access_token()
-            self.api_status = 200 #Check this one, only set if method above returns true. This one doesn work, code stops after 401. Check again when token expires
         elif self.api_status in range(402, 451):
             print(f'{datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}: 4xx client error - API Code {self.api_status}')
         elif self.api_status in range(500, 511):
